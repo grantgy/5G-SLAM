40c20c56ca773a210d4c3f14fbfc12ceeb6d4b9d
Y <47159710+grantgy@users.noreply.github.com>
2021-03-05 19:52:06 +0800

    Refactor: eliminate unnecessary blank lines
    * There is no need to have more than 1 blank line between two lines of codes.

--- PF_prediction.m
+++ PF_prediction.m
@@ -2,7 +2,6 @@ function [particle,weight] = PF_prediction(particle,weight,v,ang_v,T,Q)
     v_a = v/ang_v;
     ang_delta = ang_v * T;
     
-
     for i = 1:size(particle,2)
         particle(1,i) = particle(1,i) + v_a*(sin(particle(3,i)+ang_delta) -sin(particle(3,i)));
         particle(2,i) = particle(2,i) + v_a*(-cos(particle(3,i)+ang_delta) +cos(particle(3,i)));
