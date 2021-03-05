7f6dbe4eb3e3b2ccc90e4085648d2f73aa2fa061
Y <47159710+grantgy@users.noreply.github.com>
2021-03-05 15:35:55 +0100

    Document: add help in Cali.m
    * add the project information
    * add author's information
    * add the usage of the code
    * Introduce variables

--- Cali.m
+++ Cali.m
@@ -1,4 +1,10 @@
 function [output] = Cali(output,input)
+    % 5G mmWave Simultaneous Localization and Mapping
+    % (c) Yu Ge, 2021 (Ph.D. student at Chalmers Univerisy of Technology, emai: yuge@chalmers.se)
+    % Usage: this code generates calibrated measurement
+    % The measurement of the azimuth should consider its period 2pi
+    % input: before calibration, measurement
+    % output: after cliabration, measurement
     
     while (output(2) - input(2) > pi)
         output(2) = output(2) - 2*pi;
