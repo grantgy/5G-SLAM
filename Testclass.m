a8c7e4958571961c466dc3414a5e1fd01feebd72
Y <47159710+grantgy@users.noreply.github.com>
2021-03-09 16:16:25 +0100

    Add function to test symmetry of covariance matrix
    * test_kf_prediction_symmetrix_P tests that kf_prediction works as intended
    * It checks the covariance matrix after Kalman filter prediction step should be symmetric

--- Testclass.m
+++ Testclass.m
@@ -1,5 +1,19 @@
 classdef Testclass < matlab.unittest.TestCase
   methods (Tests)
+        function test_kf_prediction_symmetrix_P(testCase)
+            %test_kf_prediction_symmetrix_P tests that kf_prediction works as intended
+            
+            %This is to check the covariance matrix after Kalman filter prediction step 
+            % should be symmetric 
+            x = roundn(5*rand(4,1),-3);
+            P = roundn(5*abs(rand(4,4)),-3);
+            P = (P+P')/2;
+            F = roundn(5*rand(4,4),-3);
+            Q = roundn(5*abs(rand(4,4)),-3);
+            Q = (Q+Q')/2;            
+            [~,actSolution_P] = kf_prediction(x,P,F,Q);
+            testCase.verifyEqual(actSolution_P,actSolution_P');
+        end
   
   end
 end
