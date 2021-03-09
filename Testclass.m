9ca6ad9f2b8b3ab056c8c438361a1a895b9a7ce2
Y <47159710+grantgy@users.noreply.github.com>
2021-03-09 16:24:31 +0100

    Add function to test dimensions of state & matrix
    * test_kf_prediction_dimension tests that kf_prediction works as intended.
    * It checks the state and covariance matrix after Kalman filter prediction step meet the dimensional requirements.

--- Testclass.m
+++ Testclass.m
@@ -14,6 +14,27 @@ classdef Testclass < matlab.unittest.TestCase
             [~,actSolution_P] = kf_prediction(x,P,F,Q);
             testCase.verifyEqual(actSolution_P,actSolution_P');
         end
+        
+        function test_kf_prediction_dimension(testCase)
+            % test_kf_prediction_dimension tests that kf_prediction works as intended
+            
+            % This is to check the size of state and covariance matrix outputed by Kalman 
+            % filter prediction step meet the size requirement.
+            x = roundn(5*rand(4,1),-3);
+            P = roundn(5*abs(rand(4,4)),-3);
+            P = (P+P')/2;
+            F = roundn(5*rand(4,4),-3);
+            Q = roundn(5*abs(rand(4,4)),-3);
+            Q = (Q+Q')/2;
+            
+            expSolution_x_dim = [4,1];
+            expSolution_P_dim = [4,4];
+            
+            [actSolution_x,actSolution_P] = kf_prediction(x,P,F,Q);
+
+            testCase.verifySize (actSolution_P,expSolution_P_dim);
+            testCase.verifySize (actSolution_x,expSolution_x_dim);
+        end
   
   end
 end
