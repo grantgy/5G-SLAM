309a095078a33568fecbcd720a79e8eb374b75d5
Y <47159710+grantgy@users.noreply.github.com>
2021-03-09 17:25:19 +0100

    Add function to test AOD decreases as intended
    test_Cali_AOD_decrease tests that Cali.m decreases AOD as intended

--- Testclass.m
+++ Testclass.m
@@ -84,5 +84,13 @@ classdef Testclass < matlab.unittest.TestCase
             expSolution = [0;0;0;3*pi/2;0];
             testCase.verifyEqual(actSolution,expSolution);
         end
+        
+        function test_Cali_AOD_decrease(testCase)
+            % test_Cali_AOD_decrease tests that Cali.m works as intended if
+            % AOD need to be decreased
+            actSolution = Cali([0;0;0;3*pi/4;0],[0;0;0;-pi/2;0]);
+            expSolution = [0;0;0;-5*pi/4;0];
+            testCase.verifyEqual(actSolution,expSolution);
+        end
   end
 end
