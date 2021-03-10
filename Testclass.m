d8596b8eeaa836026b4aa62d02351c0170f019c0
Y <47159710+grantgy@users.noreply.github.com>
2021-03-10 04:10:18 +0100

    Add introduction to Testclass

--- Testclass.m
+++ Testclass.m
@@ -1,4 +1,7 @@
 classdef Testclass < matlab.unittest.TestCase
+  %Testclass contains test functions which test some testable functions 
+  %work as intended. The folder is added to the path and then a variety
+  %of tests are performed.
   methods (Tests)
         function test_kf_prediction_symmetrix_P(testCase)
             %test_kf_prediction_symmetrix_P tests that kf_prediction works as intended
